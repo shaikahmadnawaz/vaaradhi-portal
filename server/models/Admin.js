d7ff792b9b3f8f9131b6960420acbdb1bac69e86
Varshith-p <varshithpuligadda@gmail.com>
2023-03-07 21:09:17 +0530

    schemas created

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,1 +1,36 @@
-// TO-DO: Admin Schema
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "Name can't be empty",
+  },
+  image: {
+    type: String,
+    validate: {
+      validator: validator.isURL,
+      message: "Not a valid URL",
+    },
+  },
+  email: {
+    type: String,
+    validate: {
+      validator: validator.isEmail,
+      message: "not a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: "Date of birth can't be empty",
+    minlength: 5,
+  },
+  mobile: {
+    type: Number,
+    validate: {
+      validator: validator.isMobilePhone,
+      message: "not a valid email",
+    },
+  },
+});
+
+export default mongoose.model("Admin", adminSchema);
