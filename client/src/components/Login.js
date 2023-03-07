9b12f4301ca1aab379adcd6ed9fd9fe534ca2791
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-07 17:28:20 +0530

    login page work done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,30 +12,63 @@ const Login = () => {
           <h2 className="title my-[15px] text-blueTheme uppercase text-5xl font-bold">
             Welcome
           </h2>
-          <div className="input-div relative">
-            <div className="input-div-one mt-[0]">
-              <div className="i flex justify-center items-center">
-                <i className="fas fa-envelope "></i>
+          <div className="input-div relative grid grid-cols-14 my-[25px] py-[5px]">
+            <div className="input-div-one mt-0 flex items-center">
+              <div className="i text-blueTheme flex justify-center items-center">
+                <i className="fas fa-envelope"></i>
               </div>
-              <div class="div">
-                <h5 className="absolute">Email</h5>
-                <input type="text" class="input" />
+              <div className="input-div-two">
+                <input
+                  type="email"
+                  id="email"
+                  className="input border-b-2 border-gray-400 focus:outline-none w-full py-1 text-base ml-2"
+                  placeholder="Enter your email"
+                />
               </div>
-              <div class="input-div pass mb-[4px]">
-                <div class="i">
-                  <i class="fas fa-lock"></i>
-                </div>
-                <div class="div">
-                  <h5>Password</h5>
-                  <input type="password" class="input" />
-                </div>
+            </div>
+            <div className="input-div pass mb-4 flex items-center">
+              <div className="i text-blueTheme flex justify-center items-center">
+                <i className="fas fa-lock"></i>
+              </div>
+              <div className="input-div-two">
+                <input
+                  type="password"
+                  id="password"
+                  className="input border-b-2 border-gray-400 focus:outline-none w-full py-1 text-base ml-2"
+                  placeholder="Enter your password"
+                />
               </div>
             </div>
           </div>
 
+          <div className="flex flex-row items-start mx-4">
+            <div className="flex mx-4 items-center">
+              <input type="radio" name="login-type" id="Donor" value="Donor" />
+              <label htmlFor="Donor" className="ml-2 text-blueTheme">
+                Donor
+              </label>
+            </div>
+            <div className="flex mx-4 items-center">
+              <input type="radio" name="login-type" id="admin" value="admin" />
+              <label htmlFor="admin" className="ml-2 text-blueTheme">
+                Admin
+              </label>
+            </div>
+            <div className="flex mx-4 items-center">
+              <input
+                type="radio"
+                name="login-type"
+                id="Caretaker"
+                value="Caretaker"
+              />
+              <label htmlFor="Caretaker" className="ml-2 text-blueTheme">
+                Caretaker
+              </label>
+            </div>
+          </div>
           <input
             type="submit"
-            class="btn block w-[100%] h-[50px] border rounded-3xl outline-none text-xl uppercase my-[1rem] cursor-pointer bg-blueTheme duration-[.5s] "
+            className="btn block w-full h-12 rounded-lg uppercase my-4 cursor-pointer bg-blueTheme text-white hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
             value="Login"
           />
         </form>
