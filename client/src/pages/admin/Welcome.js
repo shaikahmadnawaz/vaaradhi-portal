a8ae5b25cbfa815ba2912614f76d986d4e19bb3e
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-11 20:03:24 +0530

    admin welcome page done

--- client/src/pages/admin/Welcome.js
+++ client/src/pages/admin/Welcome.js
@@ -1,7 +1,116 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
-  return <div>Welcome Admin</div>;
+  return (
+    <section className="w-full h-screen bg-slate-200">
+      <nav className="navbar w-full bg-slate-100 flex items-center justify-between px-4 py-4 z-10">
+        <div className="name">
+          <p className="text-black font-bold text-lg">
+            Welcome <span className="text-blueTheme">Admin</span>
+          </p>
+        </div>
+        <div className="profile flex items-center">
+          <img
+            src="https://picsum.photos/200"
+            alt="Profile"
+            className="w-10 h-10 rounded-full mr-2"
+          />
+        </div>
+      </nav>
+      <div className="flex">
+        <div className="sidebar w-1/4 bg-gray-100 h-screen">
+          <div className="p-4">
+            <h2 className="text-lg font-bold mb-4">Dashboard</h2>
+            <ul className="text-gray-800">
+              <li className="mb-2">
+                <Link href="#" className="hover:text-blueTheme">
+                  Students
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link href="#" className="hover:text-blueTheme">
+                  Donors
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link href="#" className="hover:text-blueTheme">
+                  Caretakers
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link href="#" className="hover:text-blueTheme">
+                  Announcements
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link href="#" className="hover:text-blueTheme">
+                  Donations
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="right-side w-3/4 bg-white h-full">
+          <div className="flex justify-between gap-4 p-4">
+            <div className="box flex justify-center items-center flex-col bg-blueTheme w-1/3 h-48 rounded-lg p-4">
+              <h3 className="text-white font-bold text-xl mb-2">Students</h3>
+              <p className="text-white">15</p>
+            </div>
+            <div className="box flex justify-center items-center flex-col bg-blueTheme w-1/3 h-48 rounded-lg p-4">
+              <h3 className="text-white font-bold text-xl mb-2">Donors</h3>
+              <p className="text-white">10</p>
+            </div>
+            <div className="box flex justify-center items-center flex-col bg-blueTheme w-1/3 h-48 rounded-lg p-4">
+              <h3 className="text-white font-bold text-xl mb-2">Caretakers</h3>
+              <p className="text-white">7</p>
+            </div>
+          </div>
+          <div className="p-4 h-screen">
+            <h2 className="text-lg font-bold mb-4">Announcements</h2>
+            <ul className="text-gray-800">
+              <li className="mb-2">
+                <p className="text-sm font-semibold">
+                  <span className="text-blueTheme">John Doe</span> posted a new
+                  announcement
+                </p>
+              </li>
+              <li className="mb-2">
+                <p className="text-sm font-semibold">
+                  <span className="text-blueTheme">John Doe</span> posted a new
+                  announcement
+                </p>
+              </li>
+              <li className="mb-2">
+                <p className="text-sm font-semibold">
+                  <span className="text-blueTheme">John Doe</span> posted a new
+                  announcement
+                </p>
+              </li>
+              <li className="mb-2">
+                <p className="text-sm font-semibold">
+                  <span className="text-blueTheme">John Doe</span> posted a new
+                  announcement
+                </p>
+              </li>
+              <li className="mb-2">
+                <p className="text-sm font-semibold">
+                  <span className="text-blueTheme">John Doe</span> posted a new
+                  announcement
+                </p>
+              </li>
+              <li className="mb-2">
+                <p className="text-sm font-semibold">
+                  <span className="text-blueTheme">John Doe</span> posted a new
+                  announcement
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Welcome;
--- client/src/pages/donor/AllStudents.js
+++ client/src/pages/donor/AllStudents.js
@@ -7,26 +7,32 @@ const AllStudents = () => {
   return (
     <section className="min-h-screen bg-gradient-to-t from-blue-400  to-red-400 flex flex-col items-center">
       <h1 className="pt-20 font-medium text-4xl">Students</h1>
-      <div className="student-container grid grid-cols-3 justify-self-center gap-8 h-4/5 justify-center rounded-md py-20 w-4/5 m-auto ">
+      <div className="student-container grid grid-cols-3 justify-self-center gap-8 h-5/5 justify-center rounded-md py-20 w-4/5 m-auto ">
         {students.map((student) => {
           return (
             <div
               key={student.id}
-              className="bg-white h-max flex flex-col rounded-md py-3 items-center shadow-md"
+              className="flex flex-col justify-center items-center max-w-sm rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-900"
             >
               <div className="w-32 h-32">
                 <img
                   src={student.image}
                   alt="card img"
-                  className="w-full mb-4 rounded-full"
+                  className="w-full mt-0 mb-7 rounded-full"
                 />
               </div>
-              <p className="text-lg font-bold">{student.name}</p>
-              <p>{student.gender}</p>
-              <p>{student.mobile}</p>
+              <p className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                {student.name}
+              </p>
+              <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                {student.gender}
+              </p>
+              <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                {student.mobile}
+              </p>
               <Link
                 to={`/donor/students/${student.id}`}
-                className="bg-blue-500 text-white p-2 rounded-lg"
+                className="inline-block rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal bg-blue-500 hover:bg-blue-600 text-white p-2"
               >
                 More details
               </Link>
