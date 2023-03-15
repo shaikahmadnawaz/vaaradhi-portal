c3d4a19c60e202897286c04cb225e62e433235c7
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-15 11:46:21 +0530

    student page done

---
+++ client/src/assets/donations.js
@@ -0,0 +1,34 @@
+const donations = [
+  {
+    id: 1,
+    date: "2022-01-01",
+    donor: "John Doe",
+    amount: 100,
+  },
+  {
+    id: 2,
+    date: "2022-01-05",
+    donor: "Jane Smith",
+    amount: 50,
+  },
+  {
+    id: 3,
+    date: "2022-01-10",
+    donor: "Bob Johnson",
+    amount: 25,
+  },
+  {
+    id: 4,
+    date: "2022-01-15",
+    donor: "Sara Lee",
+    amount: 75,
+  },
+  {
+    id: 5,
+    date: "2022-01-20",
+    donor: "Tom Williams",
+    amount: 200,
+  },
+];
+
+export default donations;
--- client/src/pages/admin/Student.js
+++ client/src/pages/admin/Student.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import { Link } from "react-router-dom";
+import donations from "../../assets/donations";
 
 const Student = () => {
   return (
@@ -21,195 +22,158 @@ const Student = () => {
       </nav>
       <div className="grid grid-cols-2 ">
         <div className="p1 h-screen">
-          <div className="mx-auto px-6 py-12 bg-white rounded-lg shadow-lg h-screen">
-            <div className="flex flex-col items-center">
+          <div className="bg-white mt-32 shadow-lg rounded-lg overflow-hidden w-80 mx-auto">
+            <div className="flex justify-center items-center mt-6">
               <img
+                className="w-48 h-48 rounded-full  object-cover"
                 src="https://picsum.photos/200"
-                alt="student img"
-                className="w-48 h-48 rounded-full mb-6 object-cover object-center border-2 border-blue-500"
+                alt="user"
               />
-              <h2 className="text-3xl font-semibold text-gray-800 tracking-wider">
-                John
-              </h2>
-              <p className="text-lg font-semibold text-gray-500">
-                01-01-2000 | male
-              </p>
-              <hr className="my-4 border-gray-400 w-full" />
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                <div className="flex flex-col">
-                  <span className="text-lg font-semibold text-gray-500">
-                    Mobile
-                  </span>
-                  <p className="text-lg font-medium text-gray-800">
-                    76854898547
-                  </p>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-lg font-semibold text-gray-500">
-                    Category
-                  </span>
-                  <p className="text-lg font-medium text-gray-800">
-                    semi-orphan
-                  </p>
-                </div>
-              </div>
-              <hr className="my-4 border-gray-400 w-full" />
-              <div className="flex flex-col">
-                <h3 className="text-xl font-semibold text-gray-800 mb-2">
-                  Care Taker's Details
-                </h3>
-                <div className="flex flex-col sm:flex-row">
-                  <div className="flex flex-col mr-6">
-                    <span className="text-lg font-semibold text-gray-500">
-                      Name
-                    </span>
-                    <p className="text-lg font-medium text-gray-800">Doe</p>
-                  </div>
-                  <div className="flex flex-col">
-                    <span className="text-lg font-semibold text-gray-500">
-                      Mobile
-                    </span>
-                    <p className="text-lg font-medium text-gray-800">
-                      987604896
-                    </p>
-                  </div>
-                </div>
+            </div>
+
+            <div className="p-4 flex flex-col justify-center items-center">
+              <h2 className="text-lg font-semibold">John</h2>
+              <p className="text-gray-600">Male</p>
+              <p className="text-gray-600">20-12-2002</p>
+              <p className="text-gray-600">28775438954</p>
+              <h3 className="text-md font-medium mt-4">Mother:</h3>
+              <p className="text-gray-600">John Father</p>
+              <p className="text-gray-600">Farmer</p>
+              <h3 className="text-md font-medium mt-4">Care Taker:</h3>
+              <p className="text-gray-600">Hohn</p>
+              <p className="text-gray-600">895489987056</p>
+              <h3 className="text-md font-medium mt-4">Category:</h3>
+              <p className="text-gray-600">Orphan</p>
+              <h3 className="text-md font-medium mt-4">Education:</h3>
+              <p className="text-gray-600">Primary</p>
+              <p className="text-gray-600">Year - 2003</p>
+              <h3 className="text-md font-medium mt-4">Documents:</h3>
+              <p className="text-gray-600">Aadhaar</p>
+              <p className="text-gray-600">Certificates</p>
+              <div className="table mt-4">
+                <table className="table-auto border-collapse border border-gray-400">
+                  <thead>
+                    <tr>
+                      <th className="px-4 py-2 text-left">Date</th>
+                      <th className="px-4 py-2 text-left">Donor</th>
+                      <th className="px-4 py-2 text-left">Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {donations.map((donation) => (
+                      <tr key={donation.id}>
+                        <td className="border px-4 py-2">{donation.date}</td>
+                        <td className="border px-4 py-2">{donation.donor}</td>
+                        <td className="border px-4 py-2">{donation.amount}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
         </div>
-        <div class="grid grid-rows-2 h-screen p-2">
-          <div class="donor-details h-screen flex flex-col items-center">
-            <h1 class="text-3xl font-semibold text-gray-800 tracking-wider">
-              Donor
+        <div className="p2 flex flex-1 h-screen">
+          <div className="activity mb-8 mt-20">
+            <h1 className="text-2xl flex justify-center items-center font-bold text-gray-800 mb-4">
+              Activities
             </h1>
-            <img
-              src="https://picsum.photos/200"
-              alt="student img"
-              class="w-48 h-48 rounded-full mb-6 object-cover object-center border-2 border-blue-500"
-            />
-            <h2 class="text-3xl font-semibold text-gray-800 tracking-wider">
-              John
-            </h2>
-            <p class="text-lg font-semibold text-gray-500">01-01-2000 | male</p>
-            <hr class="my-4 border-gray-400 w-full" />
-          </div>
-          <div class="caretaker-details h-screen flex flex-col items-center">
-            <h1 class="text-3xl font-semibold text-gray-800 tracking-wider">
-              Caretaker
-            </h1>
-            <img
-              src="https://picsum.photos/200"
-              alt="student img"
-              class="w-48 h-48 rounded-full mb-6 object-cover object-center border-2 border-blue-500"
-            />
-            <h2 class="text-3xl font-semibold text-gray-800 tracking-wider">
-              John
-            </h2>
-            <p class="text-lg font-semibold text-gray-500">01-01-2000 | male</p>
-            <hr class="my-4 border-gray-400 w-full" />
-          </div>
-        </div>
-      </div>
-      <div className="activity m-10">
-        <h1 className="text-2xl flex justify-center items-center font-bold text-gray-800 mb-4">
-          Activities
-        </h1>
-        <div className="cards ml-[6rem] my-3 grid grid-cols-3 gap-1">
-          <div className="card shadow-lg w-[20rem] bg-white">
-            <img
-              className="w-full h-60 object-cover rounded-t-lg"
-              src="https://picsum.photos/200"
-              alt="card"
-            />
-            <div className="p-6">
-              <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
-                Card title
-              </h5>
-              <p className="mb-4 text-base text-gray-600">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div className="card shadow-lg w-[20rem] bg-white">
-            <img
-              className="w-full h-60 object-cover rounded-t-lg"
-              src="https://picsum.photos/200"
-              alt="card"
-            />
-            <div className="p-6">
-              <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
-                Card title
-              </h5>
-              <p className="mb-4 text-base text-gray-600">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div className="card shadow-lg w-[20rem] bg-white">
-            <img
-              className="w-full h-60 object-cover rounded-t-lg"
-              src="https://picsum.photos/200"
-              alt="card"
-            />
-            <div className="p-6">
-              <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
-                Card title
-              </h5>
-              <p className="mb-4 text-base text-gray-600">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div className="card shadow-lg w-[20rem] bg-white">
-            <img
-              className="w-full h-60 object-cover rounded-t-lg"
-              src="https://picsum.photos/200"
-              alt="card"
-            />
-            <div className="p-6">
-              <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
-                Card title
-              </h5>
-              <p className="mb-4 text-base text-gray-600">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div className="card shadow-lg w-[20rem] bg-white">
-            <img
-              className="w-full h-60 object-cover rounded-t-lg"
-              src="https://picsum.photos/200"
-              alt="card"
-            />
-            <div className="p-6">
-              <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
-                Card title
-              </h5>
-              <p className="mb-4 text-base text-gray-600">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div className="card shadow-lg w-[20rem] bg-white">
-            <img
-              className="w-full h-60 object-cover rounded-t-lg"
-              src="https://picsum.photos/200"
-              alt="card"
-            />
-            <div className="p-6">
-              <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
-                Card title
-              </h5>
-              <p className="mb-4 text-base text-gray-600">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+            <div className="cards gap-x-6 my-6 grid grid-cols-2 gap-16">
+              <div className="card shadow-lg w-[20rem] bg-white">
+                <img
+                  className="w-full h-60 object-cover rounded-t-lg"
+                  src="https://picsum.photos/200"
+                  alt="card"
+                />
+                <div className="p-6">
+                  <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
+                    Activity 1
+                  </h5>
+                  <p className="mb-4 text-base text-gray-600">
+                    About the activity that student participated in
+                  </p>
+                </div>
+              </div>
+
+              <div className="card shadow-lg w-[20rem] bg-white">
+                <img
+                  className="w-full h-60 object-cover rounded-t-lg"
+                  src="https://picsum.photos/200"
+                  alt="card"
+                />
+                <div className="p-6">
+                  <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
+                    Activity 1
+                  </h5>
+                  <p className="mb-4 text-base text-gray-600">
+                    About the activity that student participated in detail
+                  </p>
+                </div>
+              </div>
+
+              <div className="card shadow-lg w-[20rem] bg-white">
+                <img
+                  className="w-full h-60 object-cover rounded-t-lg"
+                  src="https://picsum.photos/200"
+                  alt="card"
+                />
+                <div className="p-6">
+                  <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
+                    Activity 1
+                  </h5>
+                  <p className="mb-4 text-base text-gray-600">
+                    About the activity that student participated in detail
+                  </p>
+                </div>
+              </div>
+
+              <div className="card shadow-lg w-[20rem] bg-white">
+                <img
+                  className="w-full h-60 object-cover rounded-t-lg"
+                  src="https://picsum.photos/200"
+                  alt="card"
+                />
+                <div className="p-6">
+                  <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
+                    Activity 1
+                  </h5>
+                  <p className="mb-4 text-base text-gray-600">
+                    About the activity that student participated in detail
+                  </p>
+                </div>
+              </div>
+
+              <div className="card shadow-lg w-[20rem] bg-white">
+                <img
+                  className="w-full h-60 object-cover rounded-t-lg"
+                  src="https://picsum.photos/200"
+                  alt="card"
+                />
+                <div className="p-6">
+                  <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
+                    Activity 1
+                  </h5>
+                  <p className="mb-4 text-base text-gray-600">
+                    About the activity that student participated in detail
+                  </p>
+                </div>
+              </div>
+
+              <div className="card shadow-lg w-[20rem] bg-white">
+                <img
+                  className="w-full h-60 object-cover rounded-t-lg"
+                  src="https://picsum.photos/200"
+                  alt="card"
+                />
+                <div className="p-6">
+                  <h5 className="mb-2 text-xl font-medium leading-tight text-gray-800">
+                    Activity 1
+                  </h5>
+                  <p className="mb-4 text-base text-gray-600">
+                    About the activity that student participated in detail
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
